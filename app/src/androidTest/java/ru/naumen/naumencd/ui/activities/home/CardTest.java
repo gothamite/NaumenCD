03945b566667f0f867e987373c92dfb104b4a789
Alex Sorokin <aasorokin@naumen.ru>
2018-04-11 09:47:09 +0500

    Реализвоан паттерн Screen (PageObject)

--- app/src/androidTest/java/ru/naumen/naumencd/ui/activities/home/CardTest.java
+++ app/src/androidTest/java/ru/naumen/naumencd/ui/activities/home/CardTest.java
@@ -1,15 +1,8 @@
 package ru.naumen.naumencd.ui.activities.home;
 
-import android.content.Intent;
-import android.support.test.espresso.contrib.RecyclerViewActions;
-import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.support.v7.app.ActionBar;
-import android.widget.TextView;
 
 import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.MockitoAnnotations;
@@ -24,6 +17,7 @@ import ru.naumen.naumencd.di.Injector;
 import ru.naumen.naumencd.models.dbdto.PageEntity;
 import ru.naumen.naumencd.models.dbdto.interfaces.ItemEntity;
 import ru.naumen.naumencd.models.dbdto.interfaces.SimilarItemEntity;
+import ru.naumen.naumencd.page.CardPage;
 import ru.naumen.naumencd.repositories.CardRepository;
 import ru.naumen.naumencd.repositories.ListRepository;
 import ru.naumen.naumencd.ui.di.component.AppComponentMock;
@@ -34,7 +28,6 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.core.IsNot.not;
@@ -45,10 +38,10 @@ import static ru.naumen.naumencd.util.ClickableSpan.clickClickableSpan;
 
 @RunWith(AndroidJUnit4.class)
 public class CardTest {
-    private ActionBar actionBar;
     private ItemEntity itemEntity;
     private PageEntity pageEntity;
     private List<SimilarItemEntity> list;
+    private CardPage cardPage;
 
     @Inject
     CardRepository cardRepositoryMock;
@@ -56,16 +49,13 @@ public class CardTest {
     @Inject
     ListRepository listRepositoryMock;
 
-    @Rule
-    public ActivityTestRule<HomeActivity> activityActivityTestRule = new ActivityTestRule<>(HomeActivity.class, false, false);
-
     @Before
     public void setUp() {
         Injector injector = Injector.getInjector();
         AppComponentMock appComponentMock = (AppComponentMock) injector.getAppComponent();
         MockitoAnnotations.initMocks(this);
         appComponentMock.inject(this);
-
+        cardPage = new CardPage();
         list = ItemEntityFactory.createList(5);
         itemEntity = ItemEntityFactory.createItemEntity();
         int pageNumber = 0;
@@ -73,51 +63,43 @@ public class CardTest {
         pageEntity.setItems(PageItemEntityFactory.createList(10));
         pageEntity.setTotal(580);
         pageEntity.setPage(pageNumber);
-
+        initRepositories();
     }
 
     @Test
     public void onItemClick() {
-        launchActivity();
-
-        onView(withId(R.id.computers_similar))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
-
-        assertEquals(String.valueOf(actionBar.getTitle()), itemEntity.getName());
+        cardPage.launchActivity()
+                .clickOnItemWithPosition(0)
+                .checkActionBarTitle(itemEntity.getName());
     }
 
     @Test
     public void onBackPressed() {
-        launchActivity();
+        cardPage.launchActivity();
         pressBack();
-
-        assertEquals(0, activityActivityTestRule.getActivity().getFragmentManager().getBackStackEntryCount());
+        cardPage.checkBackStackEntryCount(0);
     }
 
     @Test
     public void compareFields() {
-        launchActivity();
-        onView(withId(R.id.company)).check(matches(isDisplayed())).check(matches(withText(itemEntity.getCompany().getName())));
-        onView(withId(R.id.introduced)).check(matches(isDisplayed())).check(matches(withText("16 Jan 1986")));
-        onView(withId(R.id.discontinued)).check(matches(withText("16 Jan 1986")));
-        assertEquals(String.valueOf(actionBar.getTitle()), itemEntity.getName());
-        onView(withId(R.id.image_comp)).check(matches(not(isDisplayed())));
+        cardPage.launchActivity()
+                .checkItemField(R.id.company, itemEntity.getCompany().getName())
+                .checkItemField(R.id.introduced, "16 Jan 1986")
+                .checkItemField(R.id.discontinued, "16 Jan 1986")
+                .checkActionBarTitle(itemEntity.getName())
+                .checkThatImageIsNotDisplayed();
     }
 
     @Test
     public void checkMore() {
-        launchActivity();
-        onView(withId(R.id.description)).perform(clickClickableSpan("... More"));
-        onView(withId(R.id.description)).check(matches(withText(itemEntity.getDescription() + " ")));
+        cardPage.launchActivity()
+                .clickOnClickableSpanWithText("... More")
+                .checkItemField(R.id.description, itemEntity.getDescription() + " ");
     }
 
-    private void launchActivity() {
+    private void initRepositories() {
         when(listRepositoryMock.getComputers(anyInt())).thenReturn(Observable.just(pageEntity));
         when(cardRepositoryMock.getComputer(anyInt())).thenReturn(Observable.just(itemEntity));
         when(cardRepositoryMock.getComputersSimilar(anyInt())).thenReturn(Observable.just(list));
-        activityActivityTestRule.launchActivity(new Intent());
-        onView(withId(R.id.computers_list))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
-        actionBar = activityActivityTestRule.getActivity().getSupportActionBar();
     }
 }
--- app/src/androidTest/java/ru/naumen/naumencd/ui/activities/home/ListTest.java
+++ app/src/androidTest/java/ru/naumen/naumencd/ui/activities/home/ListTest.java
@@ -1,12 +1,8 @@
 package ru.naumen.naumencd.ui.activities.home;
 
-import android.content.Intent;
-import android.support.test.espresso.contrib.RecyclerViewActions;
-import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.MockitoAnnotations;
@@ -16,25 +12,19 @@ import java.util.List;
 import javax.inject.Inject;
 
 import io.reactivex.Observable;
-import ru.naumen.naumencd.R;
 import ru.naumen.naumencd.di.Injector;
 import ru.naumen.naumencd.models.dbdto.PageEntity;
 import ru.naumen.naumencd.models.dbdto.interfaces.ItemEntity;
 import ru.naumen.naumencd.models.dbdto.interfaces.SimilarItemEntity;
+import ru.naumen.naumencd.page.ListPage;
 import ru.naumen.naumencd.repositories.CardRepository;
 import ru.naumen.naumencd.repositories.ListRepository;
 import ru.naumen.naumencd.ui.di.component.AppComponentMock;
 import ru.naumen.naumencd.util.ItemEntityFactory;
 import ru.naumen.naumencd.util.PageItemEntityFactory;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
@@ -44,6 +34,7 @@ public class ListTest {
     private PageEntity pageEntity;
     private ItemEntity itemEntity;
     private List<SimilarItemEntity> list;
+    private ListPage listPage;
 
     @Inject
     ListRepository listRepositoryMock;
@@ -51,16 +42,13 @@ public class ListTest {
     @Inject
     CardRepository cardRepositoryMock;
 
-    @Rule
-    public ActivityTestRule<HomeActivity> activityActivityTestRule = new ActivityTestRule<>(HomeActivity.class, false, false);
-
     @Before
     public void setUp() {
         Injector injector = Injector.getInjector();
         AppComponentMock appComponentMock = (AppComponentMock) injector.getAppComponent();
         MockitoAnnotations.initMocks(this);
         appComponentMock.inject(this);
-
+        listPage = new ListPage();
         list = ItemEntityFactory.createList(5);
         pageNumber = 0;
         pageEntity = new PageEntity();
@@ -68,60 +56,53 @@ public class ListTest {
         itemEntity = ItemEntityFactory.createItemEntity();
         pageEntity.setTotal(580);
         pageEntity.setPage(pageNumber);
+        initRepositories();
     }
 
     @Test
     public void onNextClick() throws Exception {
-        launchActivity();
-
+        listPage.launchActivity();
         int page = pageNumber + 1;
-        onView(withId(R.id.pages)).check(matches(withText(String.valueOf(page) + " of 58")));
-        pageEntity.setPage(pageNumber + 1);
-        onView(withId(R.id.next))
-                .check(matches(isDisplayed()))
-                .perform(click());
 
-        onView(withId(R.id.pages)).check(matches(withText(String.valueOf(page + 1) + " of 58")));
+        listPage.checkNumberOfPage(page);
+        pageEntity.setPage(pageNumber + 1);
+
+        listPage.showNextPage()
+                .checkNumberOfPage(page + 1);
     }
 
     @Test
     public void onPreviousClick() throws Exception {
         pageEntity.setPage(pageNumber + 1);
-        launchActivity();
-
+        listPage.launchActivity();
         int page = pageNumber + 2;
-        onView(withId(R.id.pages)).check(matches(withText(String.valueOf(page) + " of 58")));
-        pageEntity.setPage(pageNumber);
-        onView(withId(R.id.previous))
-                .check(matches(isDisplayed()))
-                .perform(click());
 
-        onView(withId(R.id.pages)).check(matches(withText(String.valueOf(page - 1) + " of 58")));
+        listPage.checkNumberOfPage(page);
+        pageEntity.setPage(pageNumber);
+
+        listPage.showPreviousPage()
+                .checkNumberOfPage(page - 1);
     }
 
     @Test
     public void onItemClick() throws Exception {
-        launchActivity();
-
-        onView(withId(R.id.computers_list))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
-        assertEquals(1, activityActivityTestRule.getActivity().getFragmentManager().getBackStackEntryCount());
+        listPage.launchActivity()
+                .clickOnItemWithPosition(0)
+                .checkBackStackEntryCount(1);
     }
 
     @Test
     public void compareItem() throws Exception {
-        launchActivity();
         String computerName = pageEntity.getItems().get(0).getName();
         String companyName = pageEntity.getItems().get(0).getCompanyName();
-
-        onView(withId(R.id.computers_list)).check(matches(hasDescendant(withText(computerName))));
-        onView(withId(R.id.computers_list)).check(matches(hasDescendant(withText(companyName))));
+        listPage.launchActivity()
+                .checkItemWithText(computerName)
+                .checkItemWithText(companyName);
     }
 
-    private void launchActivity() {
+    private void initRepositories() {
         when(listRepositoryMock.getComputers(anyInt())).thenReturn(Observable.just(pageEntity));
         when(cardRepositoryMock.getComputer(anyInt())).thenReturn(Observable.just(itemEntity));
         when(cardRepositoryMock.getComputersSimilar(anyInt())).thenReturn(Observable.just(list));
-        activityActivityTestRule.launchActivity(new Intent());
     }
 }
